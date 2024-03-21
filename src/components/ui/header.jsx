f3cbfbef8d462ef1abc80660488e8d50d8ddf769
yuransimao <yuransimao7@gmail.com>
2024-03-21 22:05:12 +0100

    chore:Update header

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -4,6 +4,7 @@ import { MenuResponsive } from './menuResponsive'
 function Header() {
 
     const [MenuResponViseble, setMenuResponViseble] = useState(false)
+    const PageActive = ({ isActive }) => (isActive ? `relative after:absolute after:left-0 after:bottom-[-0.8rem] after:w-full after:h-1 after:bg-blue-700 after:rounded-full` : ` relative after:transition-all after:duration-300 after:ease-in-out after:absolute after:left-0 after:bottom-[-0.8rem] after:w-0 after:h-1 hover:after:bg-blue-700 hover:after:w-full  after:rounded-full`)
 
     useEffect(() => {
         window.addEventListener("resize", function(){
@@ -12,16 +13,16 @@ function Header() {
     },[])
   return (
     <React.Fragment>
-        <div className='flex justify-between items-center py-4 px-8'>
+        <div className='flex justify-between items-center py-7 px-8 shadow-md'>
             <div>
                 <h2>Logo</h2>
             </div>
             <div>
-               <ul className='hidden lg:flex md:flex  gap-6'>
-                <li> <NavLink to='/'><h2>Home</h2></NavLink></li>
-                <li> <NavLink><h2>Services</h2></NavLink></li>
-                <li> <NavLink><h2>About</h2></NavLink></li>
-                <li> <NavLink><h2>Contact</h2></NavLink></li>
+               <ul className='hidden lg:flex md:flex  gap-6 relative'>
+                <li> <NavLink to='/'  className={PageActive}><h2>Home</h2></NavLink></li>
+                <li> <NavLink to='/service' className={PageActive}><h2>Services</h2></NavLink></li>
+                <li> <NavLink to='/about' className={PageActive}><h2>About</h2></NavLink></li>
+                <li> <NavLink to='/contact' className={PageActive}><h2>Contact</h2></NavLink></li>
                </ul>
 
                <div className='flex lg:hidden md:hidden'>
