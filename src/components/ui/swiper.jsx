ebbcd68b845f2f4614f5b52b12888ca37db8b37e
yuransimao <yuransimao7@gmail.com>
2024-03-24 19:37:33 +0100

    chore:update

--- src/components/ui/swiper.jsx
+++ src/components/ui/swiper.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {BookPlus, CarTaxiFront  } from 'lucide-react'
+import {ArrowRight } from 'lucide-react'
 import Car from '@/assets/car.png'
 import ManCleaningCar from "@/assets/Limpacar.jpg"
 import { Button } from '../';
@@ -23,111 +23,60 @@ import { Pagination,Autoplay } from 'swiper/modules';
           clickable: true,
         }}
         autoplay={{
-            delay: 2000,
+            delay: 2500,
             disableOnInteraction: false,
           }}
         modules={[Autoplay,Pagination]}
         
-        className="mySwiper h-[85vh]"
+        className="mySwiper h-[88.5vh]"
       >
+        
         <SwiperSlide>
-          <div className='h-full w-full flex px-8'>
-            <div className='flex flex-col justify-center gap-10 w-full h-full '>
-              <div>
-                <h2 className='text-4xl leading-snug '>Care That Shows,<br/> Everywhere You<br/> CarWash Go </h2>
-              </div>
+          <div className='h-full w-full flex px-8 image-1-slide relative after:h-full  after:w-full after:bg-zinc-900/50 after:absolute after:left-0 after:right-0 after:blur-lg after:z-10'>
 
-              <div className='flex  gap-3'>
-                <div className='p-2 bg-blue-500 rounded-full text-white cursor-pointer'><BookPlus /></div>
-                <div className='border-[1px] p-2 rounded-full transition-all ease-in-out duration-300 cursor-pointer hover:bg-blue-500 hover:text-white'><CarTaxiFront /></div>
-                <div className='border-[1px] p-2 rounded-full transition-all ease-in-out duration-300 cursor-pointer hover:bg-blue-500 hover:text-white'><BookPlus /></div>
-                <div className='border-[1px] p-2 rounded-full transition-all ease-in-out duration-300 cursor-pointer hover:bg-blue-500 hover:text-white'><CarTaxiFront /></div>
-              </div>
-              <div>
-                <p className='text-sm text-justify'>
-                  Our metculous care your shows in every detail ensuring <br/> that wherever you go, your vehice stands out  with its <br/> refreshed and spotiess appearance.
-                </p>
-              </div>
 
-              <div className='flex gap-6 items-center justify-start'>
-                <div><Button className='bg-blue-500 rounded-full transition-all duration-300 hover:bg-blue-700'>Book appontment</Button></div>
-                <div><Button variant="outline" className="transition-all duration-300 hover:bg-blue-500 hover:text-white rounded-full">How  we Work</Button></div>
-              </div>
-            </div>
-            <div  className='w-full flex h-full relative items-end'>
-              <div className='w-full h-full'>
-                <img src={Car} alt="Car"  className='w-full'/>
-              </div>
+            <div className='h-full w-full flex absolute z-20'>
 
-              <div className='absolute  right-32 bottom-20 bg-zinc-400/40   w-52  rounded-md'>
-
-                <div className='relative after:w-48 after:bg-zinc-800/100 after:blur-sm after:absolute after:right-32 p-2'>
-                <div className='h-24 w-48 rounded-md overflow-hidden blur-none'>
-                  <img src={ManCleaningCar} alt="ManCleaningCar" className='w-full h-full object-cover'/>
+             <div className='h-full w-full flex flex-col gap-3 justify-center'>
+                <div className='flex flex-col gap-1'>
+                  <h5 className='font-semibold text-xl text-gray-800'>Came your car </h5>
+                  <h2 className='font-bold text-4xl text-white uppercase'>It's To Come</h2>
+                  <h3 className='font-semibold text-2xl text-white' >Clean Your Car</h3>
+                  <p className='text-sm text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>cupiditate molestias est cumque illum sint, dignissimos, <br/>inventore eveniet dolores? Officia iure dicta nemo <br/></p>
                 </div>
-
-                <div  className='py-2 flex flex-col gap-2'>
-                  <div className='flex items-center justify-start gap-2'>
-                    <span className='bg-blue-400 p-1 rounded-full text-white'><BookCheck size={14}/></span>
-                    <h3 className='text-white text-base font-bold'> Exterior Wash</h3>
+                <div>
+                    <Button className="bg-blue-700 flex items-center gap-4 transition-all duration-300 hover:bg-blue-800">
+                      <h4 className='font-bold'>Explore your service</h4> 
+                    <span><ArrowRight size={14}/></span></Button>
                   </div>
-                  <p className='text-start font-medium text-white text-sm'>Thorough cleaning washing  and tire cleaning for a polished loock. </p>
+             </div>
+              <div  className='w-full flex h-full relative items-end'>
+              
+                <div className='absolute  right-36 bottom-12 bg-zinc-400/40 shadow-md  w-56  rounded-md'>
+                  <div className='relative after:w-48  p-2'>
+                  <div className='h-24 w-52 rounded-md overflow-hidden blur-none'>
+                    <img src={ManCleaningCar} alt="ManCleaningCar" className='w-full h-full object-cover'/>
+                  </div>
+                  <div  className='py-2 flex flex-col gap-2'>
+                    <div className='flex items-center justify-start gap-2'>
+                      <span className='bg-blue-400 p-1 rounded-full text-white'><BookCheck size={14}/></span>
+                      <h3 className='text-white text-base font-bold'> Exterior Wash</h3>
+                    </div>
+                    <p className='text-start font-medium text-white text-sm'>Thorough cleaning washing  and tire cleaning for a polished loock. </p>
+                  </div>
+                  </div>
                 </div>
-                </div>
+              
               </div>
-            
             </div>
+
+
           </div>
         </SwiperSlide>
-        <SwiperSlide>
-          <div className='h-full w-full flex px-8'>
-            <div className='flex flex-col justify-center gap-10 w-full h-full '>
-              <div>
-                <h2 className='text-4xl leading-snug '>Care That Shows,<br/> Everywhere You<br/> CarWash Go </h2>
-              </div>
 
-              <div className='flex  gap-3'>
-                <div className='p-2 bg-blue-500 rounded-full text-white cursor-pointer'><BookPlus /></div>
-                <div className='border-[1px] p-2 rounded-full transition-all ease-in-out duration-300 cursor-pointer hover:bg-blue-500 hover:text-white'><CarTaxiFront /></div>
-                <div className='border-[1px] p-2 rounded-full transition-all ease-in-out duration-300 cursor-pointer hover:bg-blue-500 hover:text-white'><BookPlus /></div>
-                <div className='border-[1px] p-2 rounded-full transition-all ease-in-out duration-300 cursor-pointer hover:bg-blue-500 hover:text-white'><CarTaxiFront /></div>
-              </div>
-              <div>
-                <p className='text-sm text-justify'>
-                  Our metculous care your shows in every detail ensuring <br/> that wherever you go, your vehice stands out  with its <br/> refreshed and spotiess appearance.
-                </p>
-              </div>
 
-              <div className='flex gap-6 items-center justify-start'>
-                <div><Button className='bg-blue-500 rounded-full transition-all duration-300 hover:bg-blue-700'>Book appontment</Button></div>
-                <div><Button variant="outline" className="transition-all duration-300 hover:bg-blue-500 hover:text-white rounded-full">How  we Work</Button></div>
-              </div>
-            </div>
-            <div  className='w-full flex h-full relative items-end'>
-              <div className='w-full h-full'>
-                <img src={Car} alt="Car"  className='w-full'/>
-              </div>
 
-              <div className='absolute  right-32 bottom-20 bg-zinc-400/40   w-52  rounded-md'>
-
-                <div className='relative after:w-48 after:bg-zinc-800/100 after:blur-sm after:absolute after:right-32 p-2'>
-                <div className='h-24 w-48 rounded-md overflow-hidden blur-none'>
-                  <img src={ManCleaningCar} alt="ManCleaningCar" className='w-full h-full object-cover'/>
-                </div>
-
-                <div  className='py-2 flex flex-col gap-2'>
-                  <div className='flex items-center justify-start gap-2'>
-                    <span className='bg-blue-400 p-1 rounded-full text-white'><BookCheck size={14}/></span>
-                    <h3 className='text-white text-base font-bold'> Exterior Wash</h3>
-                  </div>
-                  <p className='text-start font-medium text-white text-sm'>Thorough cleaning washing  and tire cleaning for a polished loock. </p>
-                </div>
-                </div>
-              </div>
-            
-            </div>
-          </div>
-        </SwiperSlide>
+        
         
         
       </Swiper>
