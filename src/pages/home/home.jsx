b7ebaf1c7efa1d8fbbb44f523ebd566c9c87d186
Gilson Leonel <gilson.leonel05@gmail.com>
2024-03-29 18:55:04 +0100

    Update main section and add some components

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,16 +1,13 @@
-import Main from '@/components/main/main'
-
 import React from 'react'
+import Main from '@/components/main/main'
 import { Swipers } from '@/components'
+
 function Home() {
   return (
     <React.Fragment>
-<<<<<<< HEAD
         Home
         <Main />
-=======
         <Swipers/>
->>>>>>> f18e2d24b59225f07e712b2a2ad6197506ac82cf
     </React.Fragment>
   )
 }
--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import * as Pages from '@/pages'
 import { Header } from "@/components";
+
 function Router (){
 
     return(
         <BrowserRouter>
-        <Header/>
-        <Routes>
-            <Route path="/" exact element={<Pages.Home/>}/>
-            <Route path="/service"  element={<Pages.Service/>}/>
-            <Route path="/contact"  element={<Pages.Contact/>}/>
-            <Route path="/about"  element={<Pages.About/>}/>
-        </Routes>
+            <Header/>
+            <Routes>
+                <Route path="/" exact element={<Pages.Home/>}/>
+                <Route path="/service"  element={<Pages.Service/>}/>
+                <Route path="/contact"  element={<Pages.Contact/>}/>
+                <Route path="/about"  element={<Pages.About/>}/>
+            </Routes>
         </BrowserRouter>
     )
 }
